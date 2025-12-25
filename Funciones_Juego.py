100ddbfdcf6e8fe22fe1498af77f3f2d089133dc
wassi <132829535+WassimAarab08@users.noreply.github.com>
2025-12-25 16:00:49 +0100

    Definicion de variables globales

---
+++ Funciones_Juego.py
@@ -0,0 +1,16 @@
+
+Jugadores_dicc={}
+Jugadores_respuestas={}
+Diccionario_palabras={}
+Palabra_secreta=""
+Numero_rondas=3
+Diccionario_vots={}
+
+
+def solicitar_datos():
+        print("====================== BIENVENIDO AL JUEGO DEL IMPOSTOR ====================== \n")
+        numero_participantes = int(input("Introduzca el numero de participantes => "))
+
+        for i in range(numero_participantes) :
+            pass
+
